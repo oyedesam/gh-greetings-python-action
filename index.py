3ae3e0484f0d1031239a7e31e1f5b960412fe390
oyedesam <samson.oyede@lexisnexisrisk.com>
2024-08-05 09:20:44 +0100

    update action

--- index.py
+++ index.py
@@ -5,5 +5,5 @@ def run():
     name = os.environ.get['INPUT_NAME']
     time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-    print(f'Hello, {name}! The current time is {time}.')
+    print(f"::set-output name={name}")
     print(f"::set-output time={time}")
