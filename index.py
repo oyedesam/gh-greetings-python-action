e6155494d29172f37e9c00b964d34e5b8c6410f8
oyedesam <samson.oyede@lexisnexisrisk.com>
2024-08-05 10:26:35 +0100

    remove unsed code

--- index.py
+++ index.py
@@ -5,8 +5,8 @@ def run():
     name = os.environ.get('INPUT_NAME')
     time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-    print(f"echo name={name}")
-    print(f"echo time={time}")
+    # print(f"echo name={name}")
+    # print(f"echo time={time}")
     
     # Set the outputs
     with open(os.environ['GITHUB_OUTPUT'], 'a') as output_file:
