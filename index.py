05d272860ccd92bd5e2b71a27658f1ec797c4543
oyedesam <samson.oyede@lexisnexisrisk.com>
2024-08-05 10:03:26 +0100

    exec function

--- index.py
+++ index.py
@@ -8,6 +8,8 @@ def run():
     print(f"echo name={name}")
     print(f"echo time={time}")
     
-    with open(os.environ['GITHUB_ENV'], 'a') as env_file:
-        env_file.write(f"NAME={name}\n")
-        env_file.write(f"TIME={time}\n")
+    # with open(os.environ['GITHUB_ENV'], 'a') as env_file:
+    #     env_file.write(f"NAME={name}\n")
+    #     env_file.write(f"TIME={time}\n")
+
+run()
