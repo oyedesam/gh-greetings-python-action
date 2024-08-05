987ab72f9f885d243230f6e0e499c3ca01239b24
oyedesam <samson.oyede@lexisnexisrisk.com>
2024-08-05 10:06:37 +0100

    Update script

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 
 def run():
-    name = os.environ.get['INPUT_NAME']
+    name = os.environ.get('INPUT_NAME')
     time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     print(f"echo name={name}")
