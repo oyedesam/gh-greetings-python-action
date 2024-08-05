36f11554a9b9cb865f435131595a40dc3ceefebf
oyedesam <samson.oyede@lexisnexisrisk.com>
2024-08-05 09:15:21 +0100

    update python custom action

--- index.py
+++ index.py
@@ -2,7 +2,8 @@ import os
 from datetime import datetime
 
 def run():
-    name = os.environ['INPUT_NAME']
+    name = os.environ.get['INPUT_NAME']
     time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
     print(f'Hello, {name}! The current time is {time}.')
+    print(f"::set-output time={time}")
