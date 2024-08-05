b4c63f3c1c94465775200fef537cfd2aa402b378
oyedesam <samson.oyede@lexisnexisrisk.com>
2024-08-05 09:29:24 +0100

    update script

--- index.py
+++ index.py
@@ -5,5 +5,5 @@ def run():
     name = os.environ.get['INPUT_NAME']
     time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-    print(f"::set-output name={name}")
-    print(f"::set-output time={time}")
+    print(f"echo name={name}")
+    print(f"echo time={time}")
