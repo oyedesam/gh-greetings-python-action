daac51872aa8459d9b3fd2732f7146c36f5410f0
oyedesam <samson.oyede@lexisnexisrisk.com>
2024-08-06 08:37:34 +0100

    Add loger

--- index.py
+++ index.py
@@ -2,11 +2,10 @@ import os
 from datetime import datetime
 
 def run():
+
+    print('Executing actions')
     name = os.environ.get('INPUT_NAME')
     time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-    # print(f"echo name={name}")
-    # print(f"echo time={time}")
     
     # Set the outputs
     with open(os.environ['GITHUB_OUTPUT'], 'a') as output_file:
